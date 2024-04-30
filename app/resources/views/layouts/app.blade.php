594693eb2918cc9639774f56737d93bab90ee282
clarissa <clae.work@gmail.com>
2024-04-30 16:50:35 +0700

    update home (masih error cssnya)

--- app/resources/views/layouts/app.blade.php
+++ app/resources/views/layouts/app.blade.php
@@ -24,8 +24,10 @@
 
     @include('layouts.partials.header')
 
+    @yield('hero')
+
     <main class="container mx-auto px-5 flex flex-grow">
-        {{$slot}}
+        {{ $slot }}
     </main>
 
     @include('layouts.partials.footer')
--- app/routes/web.php
+++ app/routes/web.php
@@ -1,10 +1,24 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', HomeController::class)->name('home');
+
+// Route::get('/', function (){
+//     return view('home');
+// });
 
 Route::middleware([
     'auth:sanctum',
