b531585b05c68196e21f5c2760648ab68d0692d2
clarissa <clae.work@gmail.com>
2024-06-05 16:53:37 +0700

    update naming web

--- app/resources/views/layouts/app.blade.php
+++ app/resources/views/layouts/app.blade.php
@@ -6,7 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <!-- Menggunakan nama aplikasi dari konfigurasi, default ke 'ESB Playbook' -->
     <title>{{ config('app.name', 'ESB Playbook') }}</title>
+
+     <link rel="icon" href="{{ asset('esbplaybook.png') }}" type="image/png"/>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
@@ -16,7 +19,6 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <!-- Styles -->
-
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script> --}}
     {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -25,38 +27,24 @@
 
     </style>
     @livewireStyles
-
 </head>
 
-<body class="font-sans antialiased ">
-
-
+<body class="font-sans antialiased">
     @include('layouts.partials.sidebar')
     {{-- <x-banner /> --}}
-
     @include('layouts.partials.header')
     @include('layouts.partials.sidebar')
-
     @yield('hero')
-
-    <main class="container mx-auto px-5 flex ;  flex-direction: row-reverse">
+    <main class="container mx-auto px-5 flex ; flex-direction: row-reverse">
         {{ $slot }}
     </main>
-
     @include('layouts.partials.footer')
-
     @stack('modals')
-
-
-
     <script>
         $(document).ready(function(e) {
             $("#toggle_btn").click(function() {
-                alert("Hi clikc");
+                alert("Hi click");
             });
-
-            //$(".header").hide();
-
         });
 
         $(document).on('click', '#toggle_btn', function() {
@@ -68,24 +56,17 @@
             });
 
             $(".header_toggle").attr('id', 'toggle-muncul');
-
-            // $("#toggle-muncul").css({'width' : '0%',
-            // });
         });
+
         $(document).on('click', '#toggle-muncul', function() {
             console.log('ccc');
             $("#nav-bar").css({
-                'width': ' 0%',
-                'margin-left': ' 0'
+                'width': '0%',
+                'margin-left': '0'
             });
             $(".header_toggle").attr('id', 'toggle_btn');
-
         });
     </script>
-
     @livewireScripts
-
 </body>
-
-
 </html>
